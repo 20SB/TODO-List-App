e63fa44f80c5666e8b7f113e81ab4f9587510b3a
20SB <subhabiswal20@gmail.com>
2023-09-15 20:52:11 +0530

    mongo atlas connected

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,19 +1,36 @@
-// require the library
+// // require the library
+// const mongoose = require('mongoose');
+
+// // connect to the database
+// mongoose.connect('mongodb://127.0.0.1/MyTodoList');
+
+// // aquire the connection (to check if it is successful)
+// const db = mongoose.connection;
+
+// // error
+// db.on('error', console.error.bind(console, "Error in connecting to MongoDB"));
+
+// // up and running then print the message
+// db.once('open', function(){
+//     console.log('Connected to Database');
+// });
+
+// // exporting the database
+// module.exports = db;
+
 const mongoose = require('mongoose');
+// mongoose.connect('mongodb://0.0.0.0:27017/placement');
+const DB = 'mongodb+srv://subhabiswal100:QAMqOf8ja2GW9oBL@cluster0.piyhowh.mongodb.net/MyTodoList';
 
-// connect to the database
-mongoose.connect('mongodb://127.0.0.1/MyTodoList');
-
-// aquire the connection (to check if it is successful)
-const db = mongoose.connection;
-
-// error
-db.on('error', console.error.bind(console, "Error in connecting to MongoDB"));
-
-// up and running then print the message
-db.once('open', function(){
-    console.log('Connected to Database');
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
-// exporting the database
-module.exports = db;
+const db = mongoose.connection;
+// error
+db.on('error',console.error.bind(console,'erroe connecting to db'));
+// up and running then message
+db.once('open',function(){
+    console.log('Success fully connected to the database')
+})
